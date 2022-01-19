ca4454f028038ce7bbc0a97288a9bef011c8be45
somej-123 <somej@naver.com>
2022-01-19 22:48:14 +0900

    생성자 오버로딩

--- CarExample.java
+++ CarExample.java
@@ -1,15 +1,25 @@
 public class CarExample {
     public static void main(String args[]) {
-        Car mycar = new Car();
+        Car car1 = new Car();
+        System.out.println("car.company : " + car1.company);
+        System.out.println();
 
-        System.out.println("제약회사 : "+ mycar.company);
-        System.out.println("모델명 : "+ mycar.model);
-        System.out.println("색깔 : "+ mycar.color);
-        System.out.println("최고속도 : "+ mycar.maxSpeed);
-        System.out.println("현재속도 : "+ mycar.speed);
+        Car car2 = new Car("자가용");
+        System.out.println("car2.company : " + car2.company);
+        System.out.println("car2.model : " + car2.model);
+        System.out.println();
 
-        //필드값 변경
-        mycar.speed = 60;
-        System.out.println("수정된 속도 : "+mycar.speed);
+        Car car3 = new Car("자가용", "빨강");
+        System.out.println("car3.company : " + car3.company);
+        System.out.println("car3.model : " + car3.model);
+        System.out.println("car3.color : " + car3.color);
+        System.out.println();
+
+        Car car4 = new Car("자가용", "검정", 200);
+        System.out.println("car4.company : " + car4.company);
+        System.out.println("car4.model : " + car4.model);
+        System.out.println("car4.color : " + car4.color);
+        System.out.println("car4.maxSpeed : " + car4.maxSpeed);
+        System.out.println();
     }
 }
